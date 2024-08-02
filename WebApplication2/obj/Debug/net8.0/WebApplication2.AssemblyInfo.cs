67410f161bed68fb79b41e23f238d9793d8bbdd8
Permyakov Roman <r.permyakov@skbkontur.ru>
2024-08-02 15:46:29 +0700

    net 8

--- WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e07e75e85d5f55ee47a1f6587f603970ecc81bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
